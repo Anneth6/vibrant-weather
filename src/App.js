e2a88028fb39ffd5df61d6f2d59f410962ca556f
Anneth6 <annrussell@outlook.com>
2025-04-13 14:16:08 +1000

    Added bootstrap, ran npm update and added legacy
    $env:NODE_OPTIONS="--openssl-legacy-provider"
    npm start

--- src/App.js
+++ src/App.js
@@ -5,30 +5,32 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded and created by{" "}
-        <a href="https://github.com/Anneth6" target="_blank" rel="noreferrer">
-          Ann Russell
-        </a>
-        , is open-sourced on{" "}
-        <a
-          href="https://github.com/Anneth6/vibrant-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://vibrant-weather.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded and created by{" "}
+          <a href="https://github.com/Anneth6" target="_blank" rel="noreferrer">
+            Ann Russell
+          </a>
+          , is open-sourced on{" "}
+          <a
+            href="https://github.com/Anneth6/vibrant-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://vibrant-weather.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
